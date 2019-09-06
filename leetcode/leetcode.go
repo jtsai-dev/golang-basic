1684ea80d87ffec381e92c67f7abcb1b3d48e651
yyeiei <yyeiei@163.com>
2019-09-06 18:39:31 +0800

    leetCode 32

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -1,9 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main() {
+func longestValidParentheses1() {
+	fmt.Println(longestValidParentheses("(()"))            // () 2
+	fmt.Println(longestValidParentheses(")()())"))         // ()() 4
+	fmt.Println(longestValidParentheses(")(())))(())())")) // (())() 6
+	fmt.Println(longestValidParentheses(")()())"))         // ()() 4
+	fmt.Println(longestValidParentheses("()(())"))         // ()(()) 6
+}
 
+// 32--
+func longestValidParentheses(s string) int {
+	maxans := 0
+	s = strings.TrimRight(strings.TrimLeft(s, ")"), "(")
+	dp := make([]int, len(s))
+	for i := 1; i < len(s); i++ {
+		if s[i] == ')' {
+			if s[i-1] == '(' {
+				if i >= 2 {
+					dp[i] = dp[i-2] + 2
+				} else {
+					dp[i] = 2
+				}
+			} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
+				dp[i] = dp[i-1] + 2
+				if (i - dp[i-1]) >= 2 {
+					dp[i] = dp[i] + dp[i-dp[i-1]-2]
+				}
+			}
+			if dp[i] > maxans {
+				maxans = dp[i]
+			}
+		}
+	}
+	return maxans
+
+	// 非连续
+	// s = strings.TrimLeft(s, ")")
+	// s = strings.TrimRight(s, "(")
+	// left, right := 0, 0
+	// for i := 0; i < len(s); i++ {
+	// 	if s[i] == '(' {
+	// 		left++
+	// 	} else {
+	// 		right++
+	// 	}
+	// }
+	// if left < right {
+	// 	return left * 2
+	// }
+
+	// return right * 2
 }
 
 func findClosestElements1() {
